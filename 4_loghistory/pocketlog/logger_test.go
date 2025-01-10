faa8ae4a8f9c38515a578a41906b0b1ea89e19f1
morcegon <eu@renanandrade.com.br>
2025-01-10 09:27:59 -0300

    add logger tests

--- 4_loghistory/pocketlog/logger_test.go
+++ 4_loghistory/pocketlog/logger_test.go
@@ -2,12 +2,66 @@ package pocketlog_test
 
 import (
 	"os"
+	"testing"
 
 	"learngo-pockets/logger/pocketlog"
 )
 
+const (
+	debugMessage = "Why wirte I still all one, ever the same,"
+	infoMessage  = "And keep invention in a noted weed,"
+	errorMessage = "That every word doth almost twell my name,"
+)
+
 func ExampleLogger_debugf() {
-	debugLogger := pocketlog.New(pocketlog.LevelDebug, os.Stdout)
+	debugLogger := pocketlog.New(pocketlog.LevelDebug, pocketlog.WithOutput(os.Stdout))
 	debugLogger.Debugf("Hello, %s", "world")
 	// Output: Hello, world
+}
+
+func TestLogger_DebugInfoErrorf(t *testing.T) {
+	type testCase struct {
+		level    pocketlog.Level
+		expected string
+	}
+
+	tt := map[string]testCase{
+		"debug": {
+			level:    pocketlog.LevelDebug,
+			expected: debugMessage + "\n" + infoMessage + "\n" + errorMessage + "\n",
+		},
+		"info": {
+			level:    pocketlog.LevenInfo,
+			expected: infoMessage + "\n" + errorMessage + "\n",
+		},
+		"error": {
+			level:    pocketlog.LevelError,
+			expected: errorMessage + "\n",
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			tw := &testWriter{}
+
+			testedLogger := pocketlog.New(tc.level, pocketlog.WithOutput(tw))
+
+			testedLogger.Debugf(debugMessage)
+			testedLogger.Infof(infoMessage)
+			testedLogger.Errorf(errorMessage)
+
+			if tw.contents != tc.expected {
+				t.Errorf("invalid contents, expected %q, got %q", tc.expected, tw.contents)
+			}
+		})
+	}
+}
+
+type testWriter struct {
+	contents string
+}
+
+func (tw *testWriter) Write(p []byte) (n int, err error) {
+	tw.contents = tw.contents + string(p)
+	return len(p), nil
 }
